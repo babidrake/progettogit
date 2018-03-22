b4f9d779578ef700cb2eabc78e46f2fdc35b8a11
barbara draghi <barbara.draghi01@universitadipavia.it>
2018-03-22 22:31:08 +0100

    Prima versione della prova:interfaccia e classe concreta restituiscono solo nome e cognome

---
+++ provagit/src/provagit/persona.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provagit;
+
+/**
+ *
+ * @author barbaradraghi
+ */
+public class persona implements personaIF{
+    private String nome;
+    private String cognome;
+    
+    @Override
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String getCognome() {
+        return cognome;
+    }
+    
+}
